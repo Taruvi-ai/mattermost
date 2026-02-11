9f5ee98040296db9814f9f15bba92dbcb68b4dc3
nehaeox <neha@eoxvantage.com>
2026-02-11 17:24:13 +0530

    Revert "removed mattermost password check and updated docker compose file."
    This reverts commit 406f2dc0e15c4b47f4d0f5f2f14db2f44ed2d7d1.

--- server/channels/app/authentication.go
+++ server/channels/app/authentication.go
@@ -427,15 +427,14 @@ func (a *App) authenticateUser(rctx request.CTX, user *model.User, password, mfa
 		return user, err
 	}
 
-	// Password check commented out - Taruvi handles authentication
-	// if err := a.CheckPasswordAndAllCriteria(rctx, user.Id, password, mfaToken); err != nil {
-	// 	if err.Id == "api.user.check_user_password.invalid.app_error" {
-	// 		rctx.Logger().LogM(mlog.MlvlLDAPInfo, "A user tried to sign in, which matched a Mattermost account, but the password was incorrect.", mlog.String("username", user.Username))
-	// 	}
+	if err := a.CheckPasswordAndAllCriteria(rctx, user.Id, password, mfaToken); err != nil {
+		if err.Id == "api.user.check_user_password.invalid.app_error" {
+			rctx.Logger().LogM(mlog.MlvlLDAPInfo, "A user tried to sign in, which matched a Mattermost account, but the password was incorrect.", mlog.String("username", user.Username))
+		}
 
-	// 	err.StatusCode = http.StatusUnauthorized
-	// 	return user, err
-	// }
+		err.StatusCode = http.StatusUnauthorized
+		return user, err
+	}
 
 	return user, nil
 }
